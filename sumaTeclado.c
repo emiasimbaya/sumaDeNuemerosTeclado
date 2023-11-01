44f0492f4a054dcce99b16dc0854ea3cb313a5b0
Emily Asimbaya <a_asimbaya@hotmail.com>
2023-11-01 16:28:19 -0500

    Final

--- sumaTeclado.c
+++ sumaTeclado.c
@@ -1,16 +1,30 @@
+// Utilizando el concepto de función, construya un programa que calcule la 
+//suma de una serie de números enteros leídos del teclado. El programa debe detenerse cuando el usuario ingrese el número 0.
 #include <stdio.h>
 #include <stdlib.h>
-int main()
-// Programa que suma dos numeros introducidos por teclado, para tomar como referncia para
-// hacer una suma de numeros infinitos hasta que el usuario ingrese 0
+
+int main (void)
 {
-    int numero1, numero2, suma;
-    printf( "Introduzca el primer numero: " );
-    scanf( "%d", &numero1 );
-    printf( "Introduzca el segundo numero: " );
-    scanf( "%d", &numero2 );
-    suma = numero1 + numero2;
-    printf( "La suma es %d", suma );
-    return 0;
+    int i, n;
+    int suma, un_numero;
+    suma = 0;
+    printf ("Ingrese la cantidad de numeros que va a ingresar:");
+    scanf ("%d", &n);
+    (void) getchar ();
+    for (i=1; i<=n; i++)
+    {
+        printf ("Suma N%d\n", i);
+        printf ("Ingresa el valor de un numero: ");
+        scanf ("%d", &un_numero);
+        (void) getchar ();
+        suma=suma+un_numero;
+        putchar ('\n');
+    }
+    printf ("Valor de suma: %d\n", suma);
+    system ("pause");
+    return EXIT_SUCCESS;
 }
+
+
+!=0 
 
